577949c0d6607844a29982493e4fe147b54ac562
sabbasova10 <sugraabbasova4@gmail.com>
2026-05-15 19:41:42 +0400

    prime

---
+++ task1.py
@@ -0,0 +1,14 @@
+def prime(num):
+    if num == 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+num = int(input("Number: "))
+
+if prime(num):
+    print("Prime")
+else:
+    print("Not prime")
